348572cd43fc05a98d6726b65532d8ff01ceb1cf
mohits-git <152606488+mohits-git@users.noreply.github.com>
2024-12-16 16:06:49 +0530

    feat: add user schema sql with goose up and down migrations

---
+++ sql/schema/001_user.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+CREATE TABLE users (
+  id UUID PRIMARY KEY,
+  name VARCHAR(255) UNIQUE NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- +goose Down
+DROP TABLE users;
